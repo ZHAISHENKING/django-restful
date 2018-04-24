98af1b97fa3398b129979bd9f3948229ea264a5d
ssh_shi <18700790825@163.com>
2018-04-24 21:24:07 +0800

    提交todu

---
+++ django_todos/task/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from task import views
+
+urlpatterns = [
+    url(r'^$',views.home),
+    url(r'^list/$',views.task_list),
+    url(r'^create/$',views.task_create),
+    url(r'^delete/(?P<pk>[0-9]+)/$',views.task_delete),
+
+]
---
+++ django_todos/task/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from task.models import Task
+# Create your views here.
+def home(request):
+	c={
+		'name':'Gali'
+	}
+	return render(request,'task/home.html',c)
+
+def task_delete(request,pk):
+	task=Task.objects.get(id=pk)
+	task.delete()
+	return HttpResponse('shanchuchenggong')
+
+def task_create(request):
+	print(request)
+	content=request.POST.get('task')
+	print(content)
+	task = Task(content=content)
+	task.save()
+	return HttpResponse('success')
+
+def task_list(request):
+	tasks = Task.objects.all()
+	c={
+		'todos':tasks
+	}
+	for task in tasks:
+		print(task.content)
+	return render(request,'task/list.html',c)
+	
